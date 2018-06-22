730826d39e3e0f79b380a5ffe9099ecc735ddebb
Richard Brooks <rickbrooks22@gmail.com>
2018-06-22 10:13:59 -0400

    added dummy data file

---
+++ dummyData.sql
@@ -0,0 +1,9 @@
+insert into todos
+	(Title, Completed)
+values
+	('Find a loan', false),
+	('Look over JS Framework', false),
+	('Look over Nodejs Info', false),
+	('Look over express', false),
+	('Look over SQL Joins' ,false)
+	;
